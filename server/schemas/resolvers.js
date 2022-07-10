d8245dd0e481a198f3398da438adcc6b5b9be9be
Csabbah <csabbah@ryerson.ca>
2022-07-10 17:47:48 -0400

    Updated server.js, resolvers.js and typeDefs with all queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,15 @@
-const {} = require('../models');
+const { Book } = require('../models');
 
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    books: async (parent) => {
+      return Book;
+    },
+  },
+  // Mutation: {},
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,46 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Book {
+    _id: ID
+    bookId: String
+    author: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+    books: Book
+  }
+
+  type Mutation {
+    logon(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      author: [String]
+      description: String
+      title: String
+      bookId: String
+      image: String
+      link: String
+    ): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
 
 module.exports = typeDefs;
